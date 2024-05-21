d520cb725895d80d0630c13132c3b002e6a2625b
joao-victor-garcia <joao.garciarp96@gmail.com>
2024-05-21 12:57:30 -0300

    Alteração de cards da home buscados por ID do usuário

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,193 +1,4 @@
-// import 'package:flutter/material.dart';
-// import 'package:cleaning_tracker/widgets/custom_progress_indicator.dart';
-// import 'package:cleaning_tracker/widgets/custom_drawer.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:cleaning_tracker/widgets/custom_service_cards_home.dart';
-
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage();
-
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-
-//   String mensagemBoasVindas = "Welcome!";
-//   @override
-//   Widget build(BuildContext context) {
-//     return FutureBuilder<String?>(
-//       future: _getDisplayName(),
-//       builder: (context, snapshot) {
-//         if (snapshot.connectionState == ConnectionState.waiting) {
-//           return CircularProgressIndicator();
-//         } else {
-//           String displayName = snapshot.data ?? "Usuário";
-//           mensagemBoasVindas = "Welcome $displayName!";
-//           return _buildContent();
-//         }
-//       },
-//     );
-//   }
-
-//   Future<String?> acessarDocumentoPorEmail(String email) async {
-//     try {
-//       CollectionReference employees =
-//           FirebaseFirestore.instance.collection('employees');
-
-//       QuerySnapshot querySnapshot =
-//           await employees.where('email', isEqualTo: email).get();
-
-//       if (querySnapshot.docs.isNotEmpty) {
-//         DocumentSnapshot documentSnapshot = querySnapshot.docs.first;
-
-//         Map<String, dynamic> data =
-//             documentSnapshot.data() as Map<String, dynamic>;
-
-//         String name = data['name'];
-
-//         return name;
-//       } else {
-//         print("Documento não encontrado para o e-mail fornecido.");
-//       }
-//     } catch (e) {
-//       print("Erro ao acessar o documento por e-mail: $e");
-//     }
-//     return null;
-//   }
-
-//   Future<String?> _getDisplayName() async {
-//     User? user = FirebaseAuth.instance.currentUser;
-//     if (user != null) {
-//       String? email = user.email;
-//       if (email != null) {
-//         return acessarDocumentoPorEmail(email);
-//       }
-//     }
-//     return null;
-//   }
-
-//   Widget _buildContent() {
-//     return Scaffold(
-//       appBar: AppBar(
-//         elevation: 20,
-//         backgroundColor: Color(0xFFB7D3ED),
-//         leading: Builder(builder: (context) {
-//           return IconButton(
-//             icon: Icon(Icons.menu),
-//             onPressed: () {
-//               Scaffold.of(context).openDrawer();
-//             },
-//           );
-//         }),
-//       ),
-//       drawer: CustomDrawer(),
-//       body: SingleChildScrollView(
-//         scrollDirection: Axis.vertical,
-//         child: Center(
-//           child: Column(
-//             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//             children: [
-//               Padding(
-//                 padding: const EdgeInsets.all(32.0),
-//                 child: SizedBox(
-//                   height: 15,
-//                 ),
-//               ),
-//               Padding(
-//                 padding: const EdgeInsets.all(32.0),
-//                 child: Row(
-//                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//                   children: [
-//                     Column(
-//                       children: [
-//                         Container(
-//                           width: 250,
-//                           child: Text(
-//                             mensagemBoasVindas,
-//                             style: TextStyle(
-//                                 fontWeight: FontWeight.bold, fontSize: 35),
-//                           ),
-//                         ),
-//                         SizedBox(height: 15),
-//                         Container(
-//                           width: 250,
-//                           child: Text(
-//                             "Check your services of the day",
-//                             style: TextStyle(fontSize: 20),
-//                             textAlign: TextAlign.justify,
-//                           ),
-//                         ),
-//                       ],
-//                     ),
-//                     SizedBox(
-//                       child: Column(
-//                         children: [CustomProgressIndicator(progresso: 1)],
-//                       ),
-//                     )
-//                   ],
-//                 ),
-//               ),
-//               Padding(
-//                 padding: const EdgeInsets.all(32.0),
-//                 child: SizedBox(
-//                   height: 10,
-//                 ),
-//               ),
-//               Column(
-//                 crossAxisAlignment: CrossAxisAlignment.start,
-//                 children: [
-//                   Padding(
-//                     padding: const EdgeInsets.all(32.0),
-//                     child: Text(
-//                       "Pending",
-//                       style: TextStyle(fontSize: 22),
-//                     ),
-//                   ),
-
-//                   SingleChildScrollView(
-//                     scrollDirection: Axis.horizontal,
-//                     child: Row(
-//                       children: <Widget>[
-//                         CustomCardHome(title: 'Deborah', hours: 2, status: 1),
-//                         CustomCardHome(title: 'Deborah', hours: 2, status: 1),
-//                         CustomCardHome(title: 'Deborah', hours: 2, status: 1),
-//                       ],
-//                     ),
-//                   ),
-//                 ],
-//               ),
-//               Column(
-//                 crossAxisAlignment: CrossAxisAlignment.start,
-//                 children: [
-//                   Padding(
-//                     padding: const EdgeInsets.all(32.0),
-//                     child: Text(
-//                       "Completed",
-//                       style: TextStyle(fontSize: 22),
-//                     ),
-//                   ),
-//                   SingleChildScrollView(
-//                     scrollDirection: Axis.horizontal,
-//                     child: Row(
-//                       children: <Widget>[
-//                         CustomCardHome(title: 'Elena', hours: 5, status: 1),
-//                         CustomCardHome(title: 'Elena', hours: 5, status: 1),
-//                       ],
-//                     ),
-//                   ),
-//                   SizedBox(height: 20)
-//                 ],
-//               ),
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-// CÓDIGO PAR
+/*
 import 'package:flutter/material.dart';
 import 'package:cleaning_tracker/widgets/custom_progress_indicator.dart';
 import 'package:cleaning_tracker/widgets/custom_drawer.dart';
@@ -243,21 +54,21 @@ class _MyHomePageState extends State<MyHomePage> {
   Future<String?> _getDisplayName() async {
     User? user = FirebaseAuth.instance.currentUser;
     if (user != null) {
-      String? email = user.email;
-      if (email != null) {
-        return acessarDocumentoPorEmail(email);
+      String? id = user.uid;
+      if (id != null) {
+        return acessarDocumentoPorID(id);
       }
     }
     return null;
   }
 
-  Future<String?> acessarDocumentoPorEmail(String email) async {
+  Future<String?> acessarDocumentoPorID(String id) async {
     try {
       CollectionReference employees =
           FirebaseFirestore.instance.collection('employees');
 
       QuerySnapshot querySnapshot =
-          await employees.where('email', isEqualTo: email).get();
+          await employees.where('id', isEqualTo: id).get();
 
       if (querySnapshot.docs.isNotEmpty) {
         DocumentSnapshot documentSnapshot = querySnapshot.docs.first;
@@ -269,7 +80,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
         return name;
       } else {
-        print("Documento não encontrado para o e-mail fornecido.");
+        print("Documento não encontrado para o ID fornecido.");
       }
     } catch (e) {
       print("Erro ao acessar o documento por e-mail: $e");
@@ -464,6 +275,274 @@ class _MyHomePageState extends State<MyHomePage> {
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return CircularProgressIndicator();
+        } else {
+          String displayName = snapshot.data ?? "Usuário";
+          mensagemBoasVindas = "Welcome $displayName!";
+          return _buildContent();
+        }
+      },
+    );
+  }
+}
+
+*/
+import 'package:flutter/material.dart';
+import 'package:cleaning_tracker/widgets/custom_progress_indicator.dart';
+import 'package:cleaning_tracker/widgets/custom_drawer.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cleaning_tracker/widgets/custom_service_cards_home.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage();
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String mensagemBoasVindas = "Welcome!";
+  String? userId; // ID do usuário atual
+  int pendingCardsCount = 0;
+  int completedCardsCount = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _getUserId(); // Obter o ID do usuário atual ao iniciar a tela
+  }
+
+  Future<void> _getUserId() async {
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      setState(() {
+        userId = user.uid;
+        print("UID do usuário logado: $userId"); // Debug: Imprime o UID do usuário logado
+      });
+    }
+  }
+
+  Future<String?> _getDisplayName() async {
+    if (userId != null) {
+      return acessarDocumentoPorId(userId!);
+    }
+    return null;
+  }
+
+  Future<String?> acessarDocumentoPorId(String id) async {
+    try {
+      print("Procurando documento com ID: $id"); // Debug: Imprime o ID do documento procurado
+      QuerySnapshot querySnapshot = await FirebaseFirestore.instance
+          .collection("employees")
+          .where("id", isEqualTo: id)
+          .get();
+      if (querySnapshot.docs.isNotEmpty) {
+        DocumentSnapshot documentSnapshot = querySnapshot.docs.first;
+        Map<String, dynamic> data =
+            documentSnapshot.data() as Map<String, dynamic>;
+        String name = data["name"];
+        print("Nome encontrado: $name"); // Debug: Imprime o nome encontrado
+        return name;
+      } else {
+        print("Documento não encontrado pelo ID fornecido: $id");
+      }
+    } catch (e) {
+      print("Erro ao acessar o documento por ID: $e");
+    }
+    return null;
+  }
+
+  Widget _buildContent() {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 20,
+        backgroundColor: Color(0xFFB7D3ED),
+        leading: Builder(builder: (context) {
+          return IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: () {
+              Scaffold.of(context).openDrawer();
+            },
+          );
+        }),
+      ),
+      drawer: CustomDrawer(),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(32.0),
+                child: SizedBox(
+                  height: 15,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(32.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      children: [
+                        Container(
+                          width: 250,
+                          child: Text(
+                            mensagemBoasVindas,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 35),
+                          ),
+                        ),
+                        SizedBox(height: 15),
+                        Container(
+                          width: 250,
+                          child: Text(
+                            "Check your services of the day",
+                            style: TextStyle(fontSize: 20),
+                            textAlign: TextAlign.justify,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      child: Column(
+                        children: [
+                          CustomProgressIndicator(
+                            progresso: (1 / 3),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(32.0),
+                child: SizedBox(
+                  height: 10,
+                ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(32.0),
+                    child: Row(
+                      children: [
+                        Text(
+                          "Pending",
+                          style: TextStyle(fontSize: 22),
+                        ),
+                        SizedBox(height: 30)
+                      ],
+                    ),
+                  ),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: StreamBuilder<QuerySnapshot>(
+                      stream: FirebaseFirestore.instance
+                          .collection("services")
+                          .where("idemployee",
+                              isEqualTo: FirebaseAuth.instance.currentUser
+                                  ?.uid) // Filtrar serviços do usuário atual
+                          .where("status",
+                              whereIn: [1, 2]) //Filtrar status 1 e 2
+                          .snapshots(),
+                      builder: (context, snapshot) {
+                        if (!snapshot.hasData) {
+                          return CircularProgressIndicator();
+                        }
+                        final services = snapshot.data!.docs;
+                        pendingCardsCount =
+                            services.length; // Atualiza pendingCardsCount
+                        print(pendingCardsCount);
+                        return Row(
+                          children: services.map<Widget>((service) {
+                            final clientName = service["clientName"];
+                            final hours = service["hour"];
+                            final status = service["status"];
+                            return CustomCardHome(
+                              title: clientName,
+                              hours: hours,
+                              status: status,
+                            );
+                          }).toList(),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(32.0),
+                    child: Row(
+                      children: [
+                        Text(
+                          "Completed",
+                          style: TextStyle(fontSize: 22),
+                        ),
+                        SizedBox(height: 30)
+                      ],
+                    ),
+                  ),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: StreamBuilder<QuerySnapshot>(
+                      stream: FirebaseFirestore.instance
+                          .collection("services")
+                          .where("idemployee",
+                              isEqualTo: FirebaseAuth.instance.currentUser
+                                  ?.uid) // Filtrar serviços do usuário atual
+                          .where("status", whereIn: [3]) //Filtrar status 3
+                          .snapshots(),
+                      builder: (context, snapshot) {
+                        if (!snapshot.hasData) {
+                          return CircularProgressIndicator();
+                        }
+                        final services = snapshot.data!.docs;
+                        completedCardsCount =
+                            services.length; // Atualiza completedCardsCount
+                        print(completedCardsCount);
+                        return Row(
+                          children: services.map<Widget>((service) {
+                            final clientName = service["clientName"];
+                            final hours = service["hour"];
+                            final status = service["status"];
+                            return CustomCardHome(
+                              title: clientName,
+                              hours: hours,
+                              status: status,
+                            );
+                          }).toList(),
+                        );
+                      },
+                    ),
+                  ),
+                  SizedBox(height: 20)
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<String?>(
+      future: _getDisplayName(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
         } else {
           String displayName = snapshot.data ?? "Usuário";
           mensagemBoasVindas = "Welcome $displayName!";
